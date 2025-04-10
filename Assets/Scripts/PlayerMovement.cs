fb20c860d5c1bbbe5f0b7337d53817ea3fd6e17c
Tạ Tuấn Anh <01689674504zx@gmail.com>
2025-04-11 00:06:51 +0700

    ...

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnMove(InputValue value)
+    {
+        Debug.Log(value.Get<Vector2>());
+    }
+}
