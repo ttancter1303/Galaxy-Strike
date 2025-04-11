7a9cda45997d34a1ef3d4a0cff406fd15894e3a5
Tạ Tuấn Anh <01689674504zx@gmail.com>
2025-04-11 20:29:11 +0700

    add rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,45 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xClapmRange = 20f;
+    [SerializeField] float yClapmRange = 10f;
 
-    // Update is called once per frame
+    [SerializeField] float controllRollFactor = 20f;
+    [SerializeField] float controllPitchFactor = 20f;
+    Vector2 movement;
+
     void Update()
     {
-        
+        ProcessTranslation();
+        ProcessRotation();
     }
+
+
+
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        movement = value.Get<Vector2>();
+    }
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * Time.deltaTime * controlSpeed;
+        float yOffset = movement.y * Time.deltaTime * controlSpeed;
+
+        float rawXPost = transform.localPosition.x + xOffset;
+        float rawYPost = transform.localPosition.y + yOffset;
+
+        float clampedXPost = Mathf.Clamp(rawXPost,-xClapmRange, xClapmRange);
+        float clampedYPost = Mathf.Clamp(rawYPost,-yClapmRange, yClapmRange);
+
+        transform.localPosition = new Vector3(clampedXPost, clampedYPost, 0f);
+    }
+    private void ProcessRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(-controllPitchFactor * movement.y, 0f, controllRollFactor * movement.x);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * 10f);
     }
 }
